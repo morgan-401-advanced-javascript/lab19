45b8b7b4a0dff0941e673b1eab6222442c1480e5
morgantatums <morgantatums@zillowgroup.com>
2019-12-17 17:44:04 -0800

    updated styling

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,3 +5,9 @@ describe('', () => {
         expect(true).toBeTruthy();
     });
 });
+// Snapshot test
+// Test that three Converters are rendered on the page
+// Test that the first converter is miles and kilometers
+// Test that the second converter is cups and fluid ounces
+// Test that the third converter is inches and centimeters
+// Test that all three Converters properly convert to and from
---
+++ src/__tests__/Converter.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+describe('', () => {
+    it('', () => {
+        expect(true).toBeTruthy();
+    });
+});
+
+// Snapshot test
+// Test that changing the x input field properly changes the state
+// Test that changing the y input field properly changes the state
+// Test that the component renders two input fields
+// Test that the labels are correctly set
